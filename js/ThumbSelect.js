c2d3b90f400af93a6fe6cc41767bea293365509e
Alexis Espósito <aesposito@innvenio.com>
2015-04-27 11:56:48 -0300

    Allow many ThumbSelect in the same html

--- js/ThumbSelect.js
+++ js/ThumbSelect.js
@@ -1,49 +1,61 @@
 (function($) {
-    var select;
-    var callback_change = null;
-    var multiple = false;
-    var select = false;
+
+    var selects = new Object();
+    var tag = null;
 
     var methods = {
         init : function(options){
-            var isSelect = select.is("select"), 
+            var isSelect = tag.is("select"), 
             items = new Array(),
-            id = select.attr('id');
+            id = tag.attr('id');
+
+            var select = {
+                id:null,
+                tag:null,
+                callback_change:null,
+                multiple:false,
+                selected:false    
+            };
 
             if (isSelect){
-                var options = select.find('option').each(function(){
+                select.tag = tag;
+                select.id = id;
+                selects[id] = select;
+                var options = selects[id].tag.find('option').each(function(){
                     var option = $(this);
                     items.push({value:option.val(), content:option.html()});
                 });
 
-                select.replaceTag('<div>', true); 
-                select.attr('id', id);
-                select = $('#' + id);
-                select.html('');
+                selects[id].tag.replaceTag('<div>', true); 
+                selects[id].tag.attr('id', id);
+                selects[id].tag = $('#' + id);
+                selects[id].tag.html('');
 
                 var appends = '';
                 for(var i = 0; i < items.length; i++){
                     var item = items[i];
-                    appends += '<span class="ThumbSelect_item" data-selected="false" data-value="' + item.value + '">' + item.content + '</span>'
+                    appends += '<span class="ThumbSelect_item" data-id="' + id + '" data-selected="false" data-value="' + item.value + '">' + item.content + '</span>'
                 }
 
-                select.html(appends);
-                $('.ThumbSelect_item').click(function(){
+                selects[id].tag.html(appends);
+                selects[id].tag.find('.ThumbSelect_item').click(function(){
                     var item = $(this);
-                    if (!multiple){
-                        $('.ThumbSelect_item').attr('data-selected', 'false');
-                        $('.ThumbSelect_item').removeClass('ThumbSelect_active');
-                        $('.ThumbSelect_item').addClass('ThumbSelect_inactive');
+                    var item_id = $(this).attr('data-id');
+                    
+                    if (!selects[item_id].multiple){
+                        selects[item_id].tag.find('.ThumbSelect_item').attr('data-selected', 'false');
+                        selects[item_id].tag.find('.ThumbSelect_item').removeClass('ThumbSelect_active');
+                        selects[item_id].tag.find('.ThumbSelect_item').addClass('ThumbSelect_inactive');
                         if (item.attr('data-selected') == 'false'){
                             item.attr('data-selected', 'true');
                             item.removeClass('ThumbSelect_inactive');
                             item.addClass('ThumbSelect_active');
-                            if (callback_change){
-                                callback_change(item.attr('data-value'));    
+                            if (selects[item_id].callback_change){
+                                selects[item_id].callback_change(item.attr('data-value'));    
                             }
                         }
                     } else {
-                        $('.ThumbSelect_item').each(function(){
+                        selects[item_id].tag.find('.ThumbSelect_item').each(function(){
                             if ($(this).attr('data-selected') == 'false'){
                                 $(this).addClass('ThumbSelect_inactive');
                             }
@@ -59,14 +71,14 @@
                             item.addClass('ThumbSelect_inactive');
                         }
 
-                        if (callback_change){
+                        if (selects[item_id].callback_change){
                             var list = new Array();
-                            $('.ThumbSelect_item').each(function(){
+                            selects[id].tag.find('.ThumbSelect_item').each(function(){
                                 if ($(this).attr('data-selected') == 'true'){
                                     list.push($(this).attr('data-value'));
                                 }
                             });
-                            callback_change(list);
+                            selects[item_id].callback_change(list);
                         }
                     }
                 });
@@ -76,17 +88,18 @@
             }
         },
         value : function(){  
-            return select.find('[data-selected="true"]').attr('data-value');
+            return tag.find('[data-selected="true"]').attr('data-value');
         }, 
         change : function(){},
         multiple : function(){},
         select : function(){
-            if (select && Array.isArray(select) && select.length > 0){
-                $('.ThumbSelect_item').removeClass('ThumbSelect_active');
-                $('.ThumbSelect_item').addClass('ThumbSelect_inactive');
-                for (var i = 0; i < select.length; i++){
-                    $('.ThumbSelect_item').each(function(){
-                        if ($(this).attr('data-value') == select[i]){
+            id = tag.attr('id');
+            if (selects[id].selected && Array.isArray(selects[id].selected) && selects[id].selected.length > 0){
+                selects[id].tag.find('.ThumbSelect_item').removeClass('ThumbSelect_active');
+                selects[id].tag.find('.ThumbSelect_item').addClass('ThumbSelect_inactive');
+                for (var i = 0; i < selected.length; i++){
+                    selects[id].tag.find('.ThumbSelect_item').each(function(){
+                        if ($(this).attr('data-value') == selected[i]){
                             $(this).attr('data-selected', 'true');
                             $(this).removeClass('ThumbSelect_inactive');
                             $(this).addClass('ThumbSelect_active');
@@ -98,14 +111,15 @@
     };
 
     $.fn.ThumbSelect = function(methodOrOptions, option) {
-        select = $(this);
+        tag = $(this);
+        id = tag.attr('id');
         if (methods[methodOrOptions]) {
             if (methodOrOptions == 'change'){
-                callback_change = option;
+                selects[id].callback_change = option;
             } else if (methodOrOptions == 'multiple'){
-                multiple = option;
+                selects[id].multiple = option;
             } else if (methodOrOptions == 'select'){
-                select = option;
+                selects[id].selected = option;
             }
 
             return methods[methodOrOptions].apply(this, Array.prototype.slice.call( arguments, 1 ));
